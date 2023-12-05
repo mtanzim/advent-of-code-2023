f5d33d4baed2bf827874be12a7016e35360786a9
Tanzim Mokammel <mtanzim@gmail.com>
2023-12-05 01:07:57 -0500

    argh

--- day-03.ts
+++ day-03.ts
@@ -12,7 +12,6 @@ const exampleInput = `
 `;
 
 type Grid = string[][];
-type Visited = boolean[][];
 type Coord = { rowIdx: number; colIdx: number };
 function parse(input: string): string[][] {
   const lines = input.split("\n").filter(Boolean);
@@ -20,7 +19,8 @@ function parse(input: string): string[][] {
 }
 
 const isNum = (c: string) => {
-  return !isNaN(Number(c));
+  const digitPattern = /\d/g;
+  return c.match(digitPattern);
 };
 
 const isSym = (c: string) => {
@@ -28,14 +28,6 @@ const isSym = (c: string) => {
 };
 
 function checkNeighbors(grid: Grid, { rowIdx, colIdx }: Coord) {
-  const len = grid.length;
-  const width = grid[0].length;
-  // visited[rowIdx][colIdx] = true;
-  const v = grid[rowIdx][colIdx];
-  if (isSym(v)) {
-    return true;
-  }
-
   const neighbors: Coord[] = [
     { rowIdx: rowIdx - 1, colIdx },
     { rowIdx: rowIdx + 1, colIdx },
@@ -45,91 +37,39 @@ function checkNeighbors(grid: Grid, { rowIdx, colIdx }: Coord) {
     { rowIdx: rowIdx + 1, colIdx: colIdx - 1 },
     { rowIdx: rowIdx - 1, colIdx: colIdx + 1 },
     { rowIdx: rowIdx - 1, colIdx: colIdx - 1 },
-  ].filter((c) => {
-    const { rowIdx, colIdx } = c;
-    return !(rowIdx < 0 || colIdx < 0 || rowIdx >= len || colIdx >= width);
+  ].filter((n) => {
+    return grid?.[n.rowIdx]?.[n.colIdx] !== undefined;
   });
 
   return neighbors.map((n) => isSym(grid[n.rowIdx][n.colIdx])).some(Boolean);
 }
 
-function dfs(grid: Grid, visited: Visited, { rowIdx, colIdx }: Coord): boolean {
-  const len = grid.length;
-  const width = grid[0].length;
-  // visited[rowIdx][colIdx] = true;
-  const v = grid[rowIdx][colIdx];
-  if (isSym(v)) {
-    return true;
-  }
-
-  const neighbors: Coord[] = [
-    { rowIdx: rowIdx - 1, colIdx },
-    { rowIdx: rowIdx + 1, colIdx },
-    { rowIdx, colIdx: colIdx - 1 },
-    { rowIdx, colIdx: colIdx + 1 },
-    { rowIdx: rowIdx + 1, colIdx: colIdx + 1 },
-    { rowIdx: rowIdx + 1, colIdx: colIdx - 1 },
-    { rowIdx: rowIdx - 1, colIdx: colIdx + 1 },
-    { rowIdx: rowIdx - 1, colIdx: colIdx - 1 },
-  ].filter((c) => {
-    const { rowIdx, colIdx } = c;
-    return !(rowIdx < 0 || colIdx < 0 || rowIdx >= len || colIdx >= width);
-  }).filter((c) => {
-    const { rowIdx, colIdx } = c;
-    return !(visited[rowIdx][colIdx] === true);
-  });
-
-  if (isNum(v)) {
-    console.log({
-      v,
-      neighbors: neighbors.map((n) => grid[n.rowIdx][n.colIdx]),
-    });
-  }
-
-  if (neighbors.length === 0) {
-    return false;
-  }
-
-  return neighbors.map((n) => dfs(grid, visited, n)).some(Boolean);
-}
-
 function pt1(grid: Grid) {
   const numbers: number[] = [];
-  let startNum = -1;
-  let endNum = -1;
-  let truthTracker: boolean[] = [];
+  let startNum: number | null = null;
 
   grid.forEach((row, rowIdx) => {
     row.forEach((col, colIdx) => {
       // neutral state
-      if (isNum(col) && endNum === -1 && startNum === -1) {
+      if (isNum(col) && startNum === null) {
         startNum = colIdx;
-        endNum = colIdx;
-      }
-      // started tracking a number
-      if (isNum(col) && startNum > -1) {
-        endNum = colIdx;
-        const isAdjacent = checkNeighbors(grid, {
-          rowIdx,
-          colIdx,
-        });
-        truthTracker.push(isAdjacent);
       }
       // number ended, reset
-      if (!isNum(col) && endNum > -1 && startNum > -1) {
-        endNum = colIdx;
-        const curNum = Number(row.slice(startNum, endNum).join(""));
-        const willAdd = truthTracker.some(Boolean);
+      if (!isNum(col) && startNum !== null) {
+        const curNum = Number(row.slice(startNum, colIdx).join(""));
+        let willAdd = false;
+        // console.log({curNum})
+        for (let i = startNum; i < colIdx; i++) {
+          willAdd = willAdd || checkNeighbors(grid, { rowIdx, colIdx: i });
+        }
         if (willAdd) {
           numbers.push(curNum);
         }
-        startNum = -1;
-        endNum = -1;
-        truthTracker = [];
+        startNum = null;
       }
     });
   });
   return numbers.reduce((acc, cur) => acc + cur, 0);
 }
-// console.log(parse(exampleInput));
 console.log(pt1(parse(exampleInput)));
+console.log(pt1(parse(Deno.readTextFileSync("inputs/day-03.txt"))));
