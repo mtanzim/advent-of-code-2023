c967fb27f79bd709e8300bbbc5ac43a3c8928703
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2023-12-06 10:11:24 -0500

    this one is freebie

--- day-06.ts
+++ day-06.ts
@@ -3,12 +3,17 @@ Time:      7  15   30
 Distance:  9  40  200
 `;
 
+const day6Input = `
+Time:        44     89     96     91
+Distance:   277   1136   1890   1768
+`;
+
 type Race = {
   time: number;
   distance: number;
 };
 
-function parse(input: string): number {
+function parse(input: string): Race[] {
   const [timesRaw, distancesRaw] = input.split("\n").filter(Boolean);
   const getNums = (raw: string) =>
     raw.replace("Time:", "").split(" ").map(Number).filter(
@@ -16,7 +21,32 @@ function parse(input: string): number {
     ).filter((n) => n > 0);
   const times = getNums(timesRaw);
   const distances = getNums(distancesRaw);
-  console.log({ times, distances, distancesRaw });
+  if (times.length !== distances.length) {
+    throw new Error("Parsing error");
+  }
+  return times.map((t, idx) => {
+    return {
+      time: t,
+      distance: distances[idx],
+    };
+  });
 }
 
-parse(exampleInput);
+function pt1(races: Race[]): number {
+  return races.map((r) => {
+    const { time, distance } = r;
+    const wins: number[] = [];
+    for (let held = 0; held < time; held++) {
+      const speed = held;
+      const timeLeft = time - held;
+      const distanceCovered = speed * timeLeft;
+      if (distanceCovered > distance) {
+        wins.push(speed);
+      }
+    }
+    return wins.length;
+  }).reduce((acc, cur) => acc * cur, 1);
+}
+
+console.log(pt1(parse(exampleInput)));
+console.log(pt1(parse(day6Input)));
