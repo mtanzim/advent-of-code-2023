22eeaceeb2a8370313f828c4de43738e71a9d306
Tanzim Mokammel <mtanzim@gmail.com>
2023-12-10 20:53:21 -0500

    day 8 part 1

--- day-08.ts
+++ day-08.ts
@@ -1,17 +1,5 @@
 console.log("hello day 8");
 
-const exampleInput = `
-RL
-
-AAA = (BBB, CCC)
-BBB = (DDD, EEE)
-CCC = (ZZZ, GGG)
-DDD = (DDD, DDD)
-EEE = (EEE, EEE)
-GGG = (GGG, GGG)
-ZZZ = (ZZZ, ZZZ)
-`;
-
 type Direction = "R" | "L";
 type Coord = string;
 type NextCoord = Record<Direction, Coord>;
@@ -37,4 +25,49 @@ function parse(input: string): Game {
   return { dirs, steps };
 }
 
-console.log(parse(exampleInput));
+function pt1(game: Game): number {
+  const { dirs, steps } = game;
+  let i = 0;
+  let counter = 0;
+  const start = "AAA";
+  const end = "ZZZ";
+
+  let coord = start;
+  while (true) {
+    if (i === dirs.length) {
+      i = 0;
+    }
+    const curDir = dirs[i];
+    coord = steps[coord][curDir];
+    counter++;
+    if (coord === end) {
+      break;
+    }
+    i++;
+  }
+  return counter;
+}
+
+const exampleInput1 = `
+RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)
+`;
+
+const exampleInput2 = `
+LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)
+`;
+
+console.log(pt1(parse(exampleInput1)));
+console.log(pt1(parse(exampleInput2)));
+console.log(pt1(parse(Deno.readTextFileSync("inputs/day-08.txt"))));
