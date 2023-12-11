8ec1ceabae66a76bc27096d99224f1b55cc57451
Tanzim Mokammel <mtanzim@gmail.com>
2023-12-10 21:40:42 -0500

    works but too slow; come back later

--- day-08.ts
+++ day-08.ts
@@ -1,4 +1,4 @@
-console.log("hello day 8");
+// console.log("hello day 8");
 
 type Direction = "R" | "L";
 type Coord = string;
@@ -48,6 +48,60 @@ function pt1(game: Game): number {
   return counter;
 }
 
+function pt1WithArgs(game: Game, start: string, end: string): number {
+  const { dirs, steps } = game;
+  let i = 0;
+  let counter = 0;
+
+  let coord = start;
+  while (true) {
+    if (i === dirs.length) {
+      i = 0;
+    }
+    const curDir = dirs[i];
+    coord = steps[coord][curDir];
+    counter++;
+    if (coord === end) {
+      break;
+    }
+    i++;
+  }
+  return counter;
+}
+
+function pt2(game: Game): number {
+  const { dirs, steps } = game;
+  let i = 0;
+  let counter = 0;
+  const starts = Object.keys(steps).filter((c) => c.endsWith("A"));
+  const ends = Object.keys(steps).filter((c) => c.endsWith("Z"));
+  console.log({ starts, ends });
+
+  if (starts.length !== ends.length) {
+    throw Error("invalid game, start and end counts mismatch");
+  }
+  const expectedEnd = "Z".repeat(starts.length);
+
+  let coords = starts;
+
+  while (true) {
+    if (i === dirs.length) {
+      i = 0;
+    }
+    const curDir = dirs[i];
+    coords = coords.map((c) => steps[c][curDir]);
+    counter++;
+    // console.log({ coords });
+    const lastChars = coords.map((c) => c.at(-1)).join("");
+
+    if (lastChars === expectedEnd) {
+      break;
+    }
+    i++;
+  }
+  return counter;
+}
+
 const exampleInput1 = `
 RL
 
@@ -68,6 +122,22 @@ BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)
 `;
 
+const exampleInput3 = `
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX
+`;
+
 console.log(pt1(parse(exampleInput1)));
 console.log(pt1(parse(exampleInput2)));
 console.log(pt1(parse(Deno.readTextFileSync("inputs/day-08.txt"))));
+
+console.log(pt2(parse(exampleInput3)));
+console.log(pt2(parse(Deno.readTextFileSync("inputs/day-08.txt"))));
