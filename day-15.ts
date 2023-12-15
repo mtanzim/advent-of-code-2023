3fbeaf2c0d31aacc9d7cfd601a9fa99c3305e42c
Tanzim Mokammel <mtanzim@gmail.com>
2023-12-15 10:15:05 -0500

    first draft

--- day-15.ts
+++ day-15.ts
@@ -45,8 +45,47 @@ function parseAllSteps(s: string): StepInstr[] {
   }, 0);
 }
 
+function traverseBoxes(steps: StepInstr[]) {
+  // try with list first, optimize after
+  const boxes: Array<Array<[string, number]> | null> = new Array(256).fill(
+    null,
+  );
+  for (const step of steps) {
+    const boxNum = hashFn(step.label);
+    const curBox = boxes[boxNum];
+    if (curBox !== null) {
+      if (step.op === "-") {
+        const curIdx = curBox.findIndex(([label, _]) => step.label === label);
+        if (curIdx === -1) {
+          continue;
+        }
+        for (let i = curIdx; curIdx < 256; i++) {
+          curBox[i] = curBox[i + 1];
+        }
+      }
+
+      if (step.op === "=") {
+        const curIdx = curBox.findIndex(([label, _]) => step.label === label);
+        if (curIdx === -1) {
+          const firstNullIdx = curBox.findIndex((lens) => lens === null);
+          curBox[firstNullIdx] = [step.label, step.focalLen!];
+        } else {
+          curBox[curIdx] = [step.label, step.focalLen!];
+        }
+      }
+    } else {
+      if (step.op === "=") {
+        boxes[boxNum] = [[step.label, step.focalLen!]];
+      }
+    }
+
+    console.log(boxes.filter((b) => b !== null));
+  }
+}
+
 // hashFn("HASH");
 const exampleInput = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`;
-console.log(parseAllSteps(exampleInput));
+// console.log(parseAllSteps(exampleInput));
+console.log(traverseBoxes(parseAllSteps(exampleInput)));
 // console.log(hashMultiple(exampleInput));
 // console.log(hashMultiple(Deno.readTextFileSync("inputs/day-15.txt")));
