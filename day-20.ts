523c52148fbf759801ce8db6cf6364fc25e76ea5
Tanzim Mokammel <mtanzim@gmail.com>
2023-12-20 11:51:29 -0700

    cp

--- day-20.ts
+++ day-20.ts
@@ -9,6 +9,7 @@ const BROADCASTER = "broadcaster";
 
 interface GameModule {
   name: string;
+  addConnection: (m: GameModule) => void;
   rx: (senderName: string, pulse: Pulse) => void;
   tx: (pulse: Pulse) => void;
 }
@@ -18,11 +19,12 @@ class FlipFlopModule implements GameModule {
   private prefix: string = FLIP_FLOP;
   name: string;
   private connections: GameModule[] = [];
-  constructor(name: string, connections: GameModule[]) {
+  constructor(name: string) {
     this.name = name;
-    this.connections = connections;
   }
-
+  addConnection(m: GameModule) {
+    this.connections.push(m);
+  }
   rx(_: string, pulse: Pulse) {
     if (pulse) {
       // nothing
@@ -32,7 +34,10 @@ class FlipFlopModule implements GameModule {
     }
   }
   tx(pulse: Pulse) {
-    this.connections.forEach((c) => c.rx(this.name, pulse));
+    this.connections.forEach((c) => {
+      console.log(`${this.name} -${pulse ? "high" : "low"} -> ${c.name}`);
+      c.rx(this.name, pulse)
+    });
   }
 }
 
@@ -41,19 +46,59 @@ class ConjunctionModule implements GameModule {
   name: string;
   private connections: GameModule[] = [];
   private state: Record<string, boolean> = {};
-  constructor(name: string, connections: GameModule[]) {
+  constructor(name: string) {
     this.name = name;
-    this.connections = connections;
-    this.connections.forEach((c) => {
-      this.state[c.name] = false;
-    });
   }
-
+  addConnection(m: GameModule) {
+    this.connections.push(m);
+    this.state[m.name] = false;
+  }
   rx(senderName: string, pulse: Pulse) {
     this.state[senderName] = pulse;
     this.tx(!Object.values(this.state).every((s) => s));
   }
   tx(pulse: Pulse) {
-    this.connections.forEach((c) => c.rx(this.name, pulse));
+    this.connections.forEach((c) => {
+      console.log(`${this.name} -${pulse ? "high" : "low"} -> ${c.name}`);
+      c.rx(this.name, pulse);
+    });
   }
 }
+
+class BroadcasterModule implements GameModule {
+  private prefix: string = BROADCASTER;
+  name: string = BROADCASTER;
+  private connections: GameModule[] = [];
+  constructor() {
+  }
+  addConnection(m: GameModule) {
+    this.connections.push(m);
+  }
+  rx(_: string, pulse: Pulse) {
+    this.tx(pulse);
+  }
+  tx(pulse: Pulse) {
+    this.connections.forEach((c) => {
+      console.log(`${this.name} -${pulse ? "high" : "low"} -> ${c.name}`);
+      c.rx(this.name, pulse);
+    });
+  }
+}
+
+function example1() {
+  const broadcaster = new BroadcasterModule();
+  const a = new FlipFlopModule("a");
+  const b = new FlipFlopModule("b");
+  const c = new FlipFlopModule("c");
+  const inv = new ConjunctionModule("inv");
+  broadcaster.addConnection(a);
+  broadcaster.addConnection(b);
+  broadcaster.addConnection(c);
+  a.addConnection(b);
+  b.addConnection(c);
+  c.addConnection(inv);
+  inv.addConnection(a);
+
+  broadcaster.rx("button module", false);
+}
+example1();
